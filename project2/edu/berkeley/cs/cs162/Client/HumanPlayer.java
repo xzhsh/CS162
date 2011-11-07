67a8b70f0287daaf0ec822fbe0e095b58baae6a8
Harvey Chang <harveychang360@gmail.com>
2011-11-06 20:22:20 -0800

    some human parsing

--- project2/edu/berkeley/cs/cs162/Client/HumanPlayer.java
+++ project2/edu/berkeley/cs/cs162/Client/HumanPlayer.java
@@ -57,6 +57,33 @@ public class HumanPlayer extends Player {
 
     @Override
     protected void handleGetMove() throws IOException {
+
+//    	ServerMessages.MakeMoveMessage moveMsg = (ServerMessages.MakeMoveMessage) m;
+//    	GameInfo gInfo  = moveMsg.getGameInfo();
+    	
+    	
+    	final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    	String humanInput = reader.readLine();
+    	if (humanInput.equals("Pass")) {
+    		byte moveCode = MessageProtocol.MOVE_PASS;
+    		Location loc = MessageFactory.createLocationInfo(0, 0);
+    		Message getMoveResp = MessageFactory.createGetMoveStatusOkMessage(moveCode, loc);
+    		connection.sendReplyToServer(getMoveResp);
+    	}
+    		//send message to worker thread that player has passed
+    	
+    	else {
+    		byte moveCode = MessageProtocol.MOVE_STONE;
+    		String[] outCoordinates = humanInput.split(" ");
+    		int xCoord = Integer.parseInt(outCoordinates[0]);
+    		int yCoord = Integer.parseInt(outCoordinates[1]);
+    		Location loc = MessageFactory.createLocationInfo(xCoord, yCoord);
+    		Message getMoveResp = MessageFactory.createGetMoveStatusOkMessage(moveCode, loc);
+    		
+    		connection.sendReplyToServer(getMoveResp);
+    		//send message to server about new move
+    	}
         //send a message to the server with byte moveType and Location loc
+    
     }
 }
