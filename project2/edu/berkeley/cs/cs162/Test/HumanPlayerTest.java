e4720e63064aece3af492d60a9599bd522502b4a
Jay Kim <jay.kim@berkeley.edu>
2011-11-07 19:48:50 -0800

    new skeletons for testing HumanPlayer and MachinePlayer

---
+++ project2/edu/berkeley/cs/cs162/Test/HumanPlayerTest.java
@@ -0,0 +1,12 @@
+package edu.berkeley.cs.cs162.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jay
+ * Date: 11/7/11
+ * Time: 7:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HumanPlayerTest {
+
+}
