cf3c5a1fab40c491b432378e38d48115b8576b70
Harvey Chang <harveychang360@gmail.com>
2011-11-08 23:04:09 -0800

    merging commit

--- project2/edu/berkeley/cs/cs162/Test/HumanPlayerTimeoutTest.java
+++ project2/edu/berkeley/cs/cs162/Test/HumanPlayerTimeoutTest.java
@@ -6,8 +6,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 
+import edu.berkeley.cs.cs162.Client.*;
+import edu.berkeley.cs.cs162.Server.*;
+import edu.berkeley.cs.cs162.Writable.*;
 
 import org.junit.Test;
+import org.junit.Test.Assert.*;
 
 import edu.berkeley.cs.cs162.Server.ClientConnection;
 
@@ -41,7 +45,23 @@ public class HumanPlayerTimeoutTest {
         ClientConnection connection = new ClientConnection(sock1, sock2, 0, new PrintStream(new NullOutputStream()));
         connection.setValid();
 
-     
+        GameInfo gameInfo = MessageFactory.createGameInfo("test_game");
+        BoardInfo boardInfo = MessageFactory.createBoardInfo(new Board(9));
+        ClientInfo cInfo = MessageFactory.createHumanPlayerClientInfo("test_human");
+        ClientInfo opp = MessageFactory.createMachinePlayerClientInfo("opponentDummy");
+
+        System.out.println("beginning message to client");
+        //send a "get move" message to MachinePlayer
+        connection.sendToClient(MessageFactory.createGetMoveMessage());
+
+        System.out.println("pause for 20 seconds");
+      
+//        Thread.sleep(30000);
+
+        //check if player gets a "game over"
+        
+        Message clientMsg = connection.readReplyFromClient(MessageFactory.createGameOverMessage(gameInfo, 1.0, 0.0, cInfo));
+        assertEquals(clientMsg.getMsgType(), MessageProtocol.OP_TYPE_GAMEOVER);
 
         sock1.close();
         sock2.close();
