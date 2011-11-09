bfc635ca41f585f965f233a14ca0fdc8f14eba7f
Jay Kim <jay.kim@berkeley.edu>
2011-11-08 20:33:54 -0800

    comments that outline what needs to be done

--- project2/edu/berkeley/cs/cs162/Test/MachinePlayerTimeoutTest.java
+++ project2/edu/berkeley/cs/cs162/Test/MachinePlayerTimeoutTest.java
@@ -48,6 +48,16 @@ public class MachinePlayerTimeoutTest {
 
         //"wfg" message from machineplayer
 
-        //send
+        //send an "ok" reply
+
+        //send a "game start" w/ params message to machineplayer and other player
+
+        //receive "ok" from both
+
+        //send a "get move" message to machineplayer
+
+        //artificially timeout
+
+        //check if player gets a "game over"
     }
 }
