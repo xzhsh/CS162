94cd6113622999daba00aeb928133c8fa3bc1ed5
Kunal Mehta <k.mehta@berkeley.edu>
2011-12-05 16:39:27 -0800

    Fixed Harvey's botched ServerStateManager merge.

--- project3/edu/berkeley/cs/cs162/Server/ServerStateManager.java
+++ project3/edu/berkeley/cs/cs162/Server/ServerStateManager.java
@@ -2,10 +2,7 @@ package edu.berkeley.cs.cs162.Server;
 
 
 import java.sql.ResultSet;
-=======
 import edu.berkeley.cs.cs162.Writable.MessageProtocol;
-
->>>>>>> f072ed04f3ac3a3bd0431b2743df2d43785c8d07
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -66,95 +63,6 @@ public class ServerStateManager {
 			}
 		
 		//throw new RuntimeException("Unimplemented Method");
-	}
-	
-	public void updateGameWithMove(Game game, ClientLogic client, BoardLocation loc, Vector<BoardLocation> capturedStones) throws SQLException {
-		String mType = Byte.toString(MessageProtocol.MOVE_PASS);
-		String gName = game.getName();
-		String pName = client.getName();
-		int xCoord = loc.getX();
-		int yCoord = loc.getY();
-		
-		String getPIDQuery = "SELECT clientID FROM clients WHERE name = '" + pName + "'";
-		ResultSet playerID = connection.executeReadQuery(getPIDQuery);
-		playerID.next();
-		String pID = Integer.toString(playerID.getInt("clientID"));
-		connection.closeReadQuery(playerID);
-		String mNum = Integer.toString(game.getBoard().getNumberOfMoves());
-		String gID = "how do I get this?";
-		String addMoveQuery = "INSERT INTO moves (clientID gameID moveType x y moveNum) VALUES (" + pID + "" + gID + "" + mType + "" + xCoord + "" + yCoord + "" + mNum + ")";
-		try {
-			connection.executeWriteQuery(addMoveQuery);
-			connection.finishTransaction();
-		}
-        catch (SQLException e) {
-        	connection.abortTransaction();
-        	
-        }
-        return;
-		// throw new RuntimeException("Unimplemented Method");
-	}
-	
-	public void updateGameWithPass(Game game, ClientLogic client) throws SQLException {
-		String mType = Byte.toString(MessageProtocol.MOVE_PASS);
-		String gName = game.getName();
-		String pName = client.getName();
-		
-		String getPIDQuery = "SELECT clientID FROM clients WHERE name = '" + pName + "'";
-		ResultSet playerID = connection.executeReadQuery(getPIDQuery);
-		playerID.next();
-		String pID = Integer.toString(playerID.getInt("clientID"));
-		connection.closeReadQuery(playerID);
-		String mNum = Integer.toString(game.getBoard().getNumberOfMoves());
-		String gID = "how do I get this?";
-		String addMoveQuery = "INSERT INTO moves (clientID gameID moveType moveNum) VALUES (" + pID + "" + gID + "" + mType + "" + mNum + ")";
-		try {
-			connection.executeWriteQuery(addMoveQuery);
-			connection.finishTransaction();
-		}
-        catch (SQLException e) {
-        	connection.abortTransaction();
-        	
-        }
-        return;
-		// throw new RuntimeException("Unimplemented Method");
-	}
-	
-	public void finishGame(int gId, ClientLogic winner, double blackScore, double whiteScore, int reasn) throws SQLException {
-		String pName = winner.getName();
-		String wIDQuery = "SELECT clientID FROM clients WHERE name = '" + pName + "'";
-		ResultSet winnerID = connection.executeReadQuery(wIDQuery);
-		winnerID.next();
-        int wID = winnerID.getInt("clientID");
-        connection.closeReadQuery(winnerID);
-        connection.startTransaction();
-        try {
-        	String finGameQuery = "UPDATE games SET winner = " + Integer.toString(wID) + ", blackScore= " + Double.toString(blackScore) + ", whiteScore= " + Double.toString(whiteScore) + ", reason= " + Integer.toString(reasn) + " WHERE gameID='" + Integer.toString(gID) + "'";
-        	connection.executeWriteQuery(finGameQuery);
-        	connection.finishTransaction();
-        }
-        catch (SQLException e) {
-        	connection.abortTransaction();
-        	
-        }
-        return;
-		// throw new RuntimeException("Unimplemented Method");
-
-        int whiteID = connection.getPlayerID(game.getWhitePlayer().getName());
-        int blackID = connection.getPlayerID(game.getBlackPlayer().getName());
-        int boardSize = game.getBoardSize();
-
-        connection.startTransaction();
-        try {
-            connection.executeWriteQuery("insert into games (blackPlayer, whitePlayer, boardSize, moveNum) values (" + blackID + ", " + whiteID + ", " + boardSize + ", 0)");
-            connection.finishTransaction();
-        }
-        catch(SQLException e){
-            connection.abortTransaction();
-            throw e;
-        }
-
-        return connection.getGameID(game);
 	}
 	
 	public void updateGameWithMove(Game game, ClientLogic client, BoardLocation loc, Vector<BoardLocation> capturedStones) throws SQLException {
